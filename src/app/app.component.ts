0581a79ef435afe99d8d04337d94d657b0a8e76e
John Breau <john.breau@vml.com>
2018-05-08 15:29:53 -0500

    ngx working

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ElectronService } from 'ngx-electron';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+
+  constructor(private _electronService: ElectronService) {}   // DI
+
+  launchWindow() {
+    this._electronService.shell.openExternal('https://coursetro.com');
+  }
+
 }
