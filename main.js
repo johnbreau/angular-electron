4a0a3dd269a2009d58627c6c71c2ad565dbc337c
John Breau <john.breau@vml.com>
2018-05-11 15:15:57 -0500

    added native menu

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const {app, BrowserWindow} = require('electron')
+const {app, Menu, BrowserWindow} = require('electron')
 const path = require('path')
 const url = require('url')
 
@@ -6,11 +6,11 @@ let win
 
 function createWindow () {
   win = new BrowserWindow({
-    frame: false,
-    height: 700,
+    frame: true,
+    width: 800,
+    height: 600,
     'standard-window': false,
     resizable: false,
-    width: 368,
   })
 
   // load the dist folder from Angular
@@ -26,6 +26,61 @@ function createWindow () {
   win.on('closed', () => {
     win = null
   })
+
+  const template = [
+    {
+      label: 'Main Menu',
+      submenu: [
+        {role: 'quit'}
+      ]
+    },
+    {
+      label: 'Edit',
+      submenu: [
+        {role: 'undo'},
+        {role: 'redo'},
+        {type: 'separator'},
+        {role: 'cut'},
+        {role: 'copy'},
+        {role: 'paste'},
+        {role: 'pasteandmatchstyle'},
+        {role: 'delete'},
+        {role: 'selectall'}
+      ]
+    },
+    {
+      label: 'View',
+      submenu: [
+        {role: 'reload'},
+        {role: 'forcereload'},
+        {role: 'toggledevtools'},
+        {type: 'separator'},
+        {role: 'resetzoom'},
+        {role: 'zoomin'},
+        {role: 'zoomout'},
+        {type: 'separator'},
+        {role: 'togglefullscreen'}
+      ]
+    },
+    {
+      role: 'window',
+      submenu: [
+        {role: 'minimize'},
+        {role: 'close'}
+      ]
+    },
+    {
+      role: 'help',
+      submenu: [
+        {
+          label: 'Learn More',
+          click () { require('electron').shell.openExternal('https://electronjs.org') }
+        }
+      ]
+    }
+  ];
+
+  Menu.setApplicationMenu(Menu.buildFromTemplate(template));
 }
 
 app.on('ready', createWindow)
