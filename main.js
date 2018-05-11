17e298387c450641aa0f44179ad4db0060697876
John Breau <john.breau@vml.com>
2018-05-11 10:11:39 -0500

    added properties for browser window

--- main.js
+++ main.js
@@ -5,7 +5,13 @@ const url = require('url')
 let win
 
 function createWindow () {
-  win = new BrowserWindow({width: 800, height: 600})
+  win = new BrowserWindow({
+    frame: false,
+    height: 700,
+    'standard-window': false,
+    resizable: false,
+    width: 368,
+  })
 
   // load the dist folder from Angular
   win.loadURL(url.format({
